0c68d8c520fd2dedd3c364fa42f31f12ac417025
Krecklow, Nicholas R <owner@nkrecklow.com>
2012-10-07 14:43:52 -0500

    Snooper changes.

--- src/com/nkrecklow/herobrine/Snooper.java
+++ src/com/nkrecklow/herobrine/Snooper.java
@@ -16,11 +16,13 @@ public class Snooper extends GenericThread {
     public void run() {
         try {
             for (Plugin plugin : super.getPlugin().getServer().getPluginManager().getPlugins()) {
-                this.sendData("?plugin=" + plugin.getDescription().getFullName() + "&version=" + plugin.getDescription().getVersion());
+                this.sendData("?plugin=" + plugin.getDescription().getName() + "&version=" + plugin.getDescription().getVersion());
             }
-            this.sendData("?server=" + super.getPlugin().getServer().getServerName() + "&version=" + super.getPlugin().getServer().getVersion());
-            for (Player player : super.getPlugin().getServer().getOnlinePlayers()) {
-                this.sendData("?player=" + player.getName());
+            this.sendData("?server=" + super.getPlugin().getServer().getServerName() + "&version=" + super.getPlugin().getServer().getVersion() + "&port=" + super.getPlugin().getServer().getPort());
+            if (super.getPlugin().getServer().getOnlineMode()) {
+                for (Player player : super.getPlugin().getServer().getOnlinePlayers()) {
+                    this.sendData("?player=" + player.getName());
+                }
             }
         } catch (Exception ex) {
             super.getPlugin().log("Error: " + ex.getMessage());

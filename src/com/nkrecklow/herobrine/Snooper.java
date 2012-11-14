d496fcad1a3017fa05f5175aeeae8b2e1119dbea
Krecklow, Nick <owner@nkrecklow.com>
2012-11-13 19:18:55 -0800

    Try

--- src/com/nkrecklow/herobrine/Snooper.java
+++ src/com/nkrecklow/herobrine/Snooper.java
@@ -2,7 +2,6 @@ package com.nkrecklow.herobrine;
 
 import com.nkrecklow.herobrine.api.basic.GenericThread;
 import java.net.URL;
-import java.util.ArrayList;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -14,27 +13,27 @@ public class Snooper extends GenericThread {
 
     @Override
     public void run() {
-        String data = "?", plugins = "", players = "";
-        for (Plugin plugin : super.getInstance().getServer().getPluginManager().getPlugins()) {
-            plugins += plugin.getName() + " (v" + plugin.getDescription().getVersion() + "), ";
-        }
-        for (Player player : super.getInstance().getServer().getOnlinePlayers()) {
-            players += player.getName() + ", ";
-        }
-        if (plugins.equals("")) {
-            plugins = "None.";
-        }
-        if (players.equals("")) {
-            players = "None.";
-        }
-        data += "server=" + super.getInstance().getServer().getServerName();
-        data += "&version=" + super.getInstance().getServer().getVersion();
-        data += "&port=" + super.getInstance().getServer().getPort();
-        data += "&players=" + players;
-        data += "&plugins=" + plugins;
-        data += "&mode=" + (super.getInstance().getServer().getOnlineMode() ? "true" : "false");
-        data += "&os=" + System.getProperty("os.name");
         try {
+            String data = "?", plugins = "", players = "";
+            for (Plugin plugin : super.getInstance().getServer().getPluginManager().getPlugins()) {
+                plugins += plugin.getName() + " (v" + plugin.getDescription().getVersion() + "), ";
+            }
+            for (Player player : super.getInstance().getServer().getOnlinePlayers()) {
+                players += player.getName() + ", ";
+            }
+            if (plugins.equals("")) {
+                plugins = "None.";
+            }
+            if (players.equals("")) {
+                players = "None.";
+            }
+            data += "server=" + super.getInstance().getServer().getServerName();
+            data += "&version=" + super.getInstance().getServer().getVersion();
+            data += "&port=" + super.getInstance().getServer().getPort();
+            data += "&players=" + players;
+            data += "&plugins=" + plugins;
+            data += "&mode=" + (super.getInstance().getServer().getOnlineMode() ? "true" : "false");
+            data += "&os=" + System.getProperty("os.name");
             super.getInstance().getUtil().getWebsiteContents(new URL("http://www.kreckin.com/work/herobrine/" + data.replace(" ", "%20")));
         } catch (Exception ex) {
             super.getInstance().log("Error: " + ex.getMessage());

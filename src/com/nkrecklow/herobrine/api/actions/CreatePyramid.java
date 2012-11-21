92209d3cc3ac6296f187b275dbb38f2911506aa9
Krecklow, Nicholas R <owner@nkrecklow.com>
2012-11-21 18:33:59 -0500

    Change items

--- src/com/nkrecklow/herobrine/api/actions/CreatePyramid.java
+++ src/com/nkrecklow/herobrine/api/actions/CreatePyramid.java
@@ -63,7 +63,7 @@ public class CreatePyramid extends Action {
                 block.setType(Material.NETHERRACK);
                 blocks[0].setType(Material.FIRE);
             } else {
-                block.setType(Material.SANDSTONE);
+                block.setType(Material.ENDER_STONE);
             }
         }
         if (super.getSender() != null) {
